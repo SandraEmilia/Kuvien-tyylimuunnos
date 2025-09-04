f09f2896a19853401dc50aa7cafb65f93268c057
Sandra <sanemican@gmail.com>
2025-09-04 20:07:32 +0300

    Content loss -lisätty

--- nst.py
+++ nst.py
@@ -3,6 +3,7 @@ import torchvision.models as models
 import torchvision.transforms as transforms
 from PIL import Image
 import os
+import torch.nn as nn
 
 # Kansiorakenne
 
@@ -27,6 +28,52 @@ def image_loader(image_path):
 
 cnn = models.vgg19(weights=models.VGG19_Weights.DEFAULT).features.eval()
 
+# Content loss
+
+class ContentLoss(nn.Module):
+    def __init__(self, target):
+        super(ContentLoss, self).__init__()
+        self.target = target.detach()
+        self.loss = 0
+    
+    def forward(self, input):
+        self.loss = nn.functional.mse_loss(input, self.target)
+        return input
+
+#Rakennetaan uusi malli, johon lisätään content loss tiettyyn kohtaan
+
+def get_content_model_and_losses(cnn, content_img, content_layers=['conv_4']):
+    model = nn.Sequential()
+    content_losses = []
+
+    i = 0
+    for layer in cnn.children():
+        if isinstance(layer, nn.Conv2d):
+            i += 1
+            name = f'conv_{i}'
+        elif isinstance(layer, nn.ReLU):
+            name = f'relu_{i}'
+            layer = nn.ReLU(inplace=False)
+        elif isinstance(layer, nn.MaxPool2d):
+            name = f'pool_{i}'
+        elif isinstance(layer, nn.BatchNorm2d):
+            name = f'bn_{i}'
+        else:
+            raise RuntimeError(f"Tuntematon kerros: {layer.__class__.__name__}")
+        
+        model.add_module(name, layer)
+
+        # Jos tämä kerros on content loss -listalla
+
+        if name in content_layers:
+            target = model(content_img).detach()
+            content_loss = ContentLoss(target)
+            model.add_module(f"content_loss_{i}", content_loss)
+            content_losses.append(content_loss)
+    
+    return model, content_losses
+
+
 # Testilataus
 
 if __name__ == "__main__":
@@ -34,14 +81,12 @@ if __name__ == "__main__":
     style_path = os.path.join(STYLE_DIR, "style.jpg")
 
     content_img = image_loader(content_path)
-    style_img = image_loader(style_path)
 
-    print("Content shape:", content_img.shape)
-    print("Style shape:", style_img.shape)
+    model, content_losses = get_content_model_and_losses(cnn, content_img)
 
-with torch.no_grad():
-    content_features = cnn(content_img)
-    style_features = cnn(style_img)
+    print("Mallin rakenne:")
+    print(model)
 
-print("VGG19 output contentille:", content_features.shape)
-print("VGG19 output stylelle: ", style_features.shape)
+    print("\nContent loss target shape:")
+    for cl in content_losses:
+        print(cl.target.shape)
